a8b4f7f89f1a3366a8418923ebada3e100a39532
root <root@localhost.localdomain>
2019-12-01 10:46:03 +0700

    Latihan Navigation dan Mengirim data

---
+++ app/src/main/java/com/svafvel/software/production/myapplication/DetailCategoryFragment.java
@@ -0,0 +1,48 @@
+package com.svafvel.software.production.myapplication;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailCategoryFragment extends Fragment {
+
+
+    public DetailCategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle saveInstanceState){
+
+        super.onViewCreated(view, saveInstanceState);
+        TextView tvCatagoryName = view.findViewById(R.id.tv_category_name);
+        TextView tvCategoryDescription = view.findViewById(R.id.tv_category_description);
+
+        String dataName = getArguments().getString(CategoryFragment.EXTRA_NAME);
+        long dataDescription = getArguments().getLong(CategoryFragment.EXTRA_STOCK);
+
+        tvCatagoryName.setText(dataName);
+        tvCategoryDescription.setText("Stock : " + dataDescription);
+
+    }
+
+}
---
+++ app/src/main/java/com/svafvel/software/production/myapplication/HomeFragment.java
@@ -0,0 +1,54 @@
+package com.svafvel.software.production.myapplication;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_catagory);
+        Button btnProfile = view.findViewById(R.id.btn_profile);
+
+        btnCategory.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment));
+        btnProfile.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_profileActivity);
+            }
+        });
+
+    }
+
+}
